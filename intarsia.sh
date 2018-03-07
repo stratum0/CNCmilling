cb538d5fea0c133b228af8c03e69f2a15c60adf9
Drahflow <drahflow@gmx.de>
2018-03-08 00:12:53 +0100

    More configurability

--- intarsia.sh
+++ intarsia.sh
@@ -5,11 +5,12 @@ set -ue
 # Produce two-part cuts for intarsia. The tool size and blur radius
 # have been tested on our "1.6mm" Dremel HSS end mills.
 
-SIZE=40
 NAME="$1"
+SIZE="${SIZE:-40}"
+PRECISION="${PRECISION:-0.05}"
 
-./depthmap2gcode.py --depth 3 --width $SIZE --height $SIZE --planes 1 --zspace 2 \
-  --cutdepth 3.2 --precision 0.05 --tool 1.4:"$NAME"-background.g \
+./depthmap2gcode.py --depth 3 --width "$SIZE" --height "$SIZE" --planes 1 --zspace 2 \
+  --cutdepth 3.2 --precision "$PRECISION" --tool 1.4:"$NAME"-background.g \
   --result "$NAME"-background-cut.png \
   "$NAME".png
 
@@ -17,8 +18,8 @@ convert "$NAME"-background-cut.png -flop "$NAME"-background-cut-mirror.png
 convert "$NAME"-background-cut-mirror.png -blur 6x3 -level 253,254 -colors 2 \
   "$NAME"-background-cut-padded.png
 
-./depthmap2gcode.py --depth 3 --width $SIZE --height $SIZE --planes 1 --zspace 2 \
-  --cutdepth 3.2 --precision 0.05 --tool 1.4:"$NAME"-foreground.g \
+./depthmap2gcode.py --depth 3 --width "$SIZE" --height "$SIZE" --planes 1 --zspace 2 \
+  --cutdepth 3.2 --precision "$PRECISION" --tool 1.4:"$NAME"-foreground.g \
   --result "$NAME"-foreground-cut.png \
   --inverse "$NAME"-background-cut-padded.png
 
